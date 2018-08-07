0788565f13596cf293af801bc4cb0f315c19f85b
veenakamath <vaikamath123@gmail.com>
2018-08-07 16:48:45 +0530

    Uodated copy.

--- Rpise_dstream/src/rpise_dstream/Rpise_dstream.java
+++ Rpise_dstream/src/rpise_dstream/Rpise_dstream.java
@@ -8,30 +8,19 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Rpise_dstream {
-    public static   ArrayList<StreamingRoom> sm = new ArrayList<StreamingRoom>();
+
+    public static ArrayList<StreamingRoom> sm = new ArrayList<StreamingRoom>();
+
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
- 
+
         System.out.println(" Server signing On :");
         ServerSocket soc = new ServerSocket(8096);
-      // accept client socket
-        //  Socket ss= soc.accept();
-
-// create a array of rooms;
-// loop through array to open socket
-// start conversation.
- //      for (int i = 0; i < 5; i++)
- //      {
- //          sm.add(new  StreamingRoom(ss) );
- //      }
-
         StreamingRoom r2 = new StreamingRoom(soc);
-         r2.setRoomName("Room2");
-        //r2.init(soc);
+        r2.setRoomName("Room2");
         r2.start();
         StreamingRoom r1 = new StreamingRoom(soc);
         r1.setRoomName("Room1");
-        //r1.init(soc);
         r1.start();
 
         try {
